26061f5c68a6684474111bc05ea934038be94612
VijayPatil0046 <vijaypatil00046@gmail.com>
2025-10-20 00:09:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+let userScore = 0;
+let compScore = 0;
+
+let msg = document.querySelector("#msg");
+let userPara = document.querySelector("#user-score");
+let compPara = document.querySelector("#computer-score");
+
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
+
+let drawGame = () =>{
+    msg.innerText = "It's a draw";
+    msg.style.backgroundColor = "#081b31";
+}
+
+let playGame = (userChoice) =>{
+    let compChoice = genCompChoice();
+    let userWin = true;
+    if(userChoice === compChoice){
+        drawGame();
+    }
+    else{
+        if (userChoice === "rock") {
+            userWin = compChoice === "scissor";
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "rock";
+        } else if (userChoice === "scissor") {
+            userWin = compChoice === "paper";
+        }
+        showWinner(userWin,userChoice,compChoice);
+    }
+    
+    
+}
+
+let showWinner = (userWin,userChoice,compChoice) =>{
+    if(userWin){
+        msg.innerText = `You win!. Computer choice: ${compChoice}`;
+        userScore++;
+        userPara.innerText = userScore;
+        msg.style.backgroundColor = "green";
+    }
+    else{
+        msg.innerText = `You lose. Computer choice: ${compChoice}`;
+        compScore++;
+        compPara.innerText = compScore;
+        msg.style.backgroundColor = "red";
+    }
+}
+
+
+// generate computer choice
+let genCompChoice = () =>{
+    const options = ["rock", "paper", "scissor"];
+    let randIndex = Math.floor(Math.random() * 3)
+    return options[randIndex];
+}
+
+let modeBtn = document.querySelector("#mode");
+let body = document.body;
+let darkMode = false;
+
+
+modeBtn.addEventListener('click',() =>{
+  darkMode = !darkMode; // toggle boolean
+  if (darkMode) {
+    modeBtn.innerText = "Light Mode";
+    body.classList.add("darkModes");
+    body.classList.remove("lightModes");
+  } else {
+    modeBtn.innerText = "Dark Mode";
+    body.classList.remove("darkModes");
+    body.classList.add("lightModes");
+  }
+});
